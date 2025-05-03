89ffca8e42c6b4e4cb3f361536de135f95039cb6
Whitenessly <whitenessly@gmail.com>
2025-05-03 21:17:48 +0700

    .

---
+++ menu/script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        window.scrollTo({
+            top: targetElement.offsetTop - 70,
+            behavior: 'smooth'
+        });
+    });
+});
